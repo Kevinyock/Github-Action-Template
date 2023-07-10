e37a7aa46614049e04c745eceb25ce13910ed072
Kevin <Westminsterkevindo@gmail.com>
2023-07-09 19:24:57 -0700

    now we can read version_number files

--- utilities/checkversion.py
+++ utilities/checkversion.py
@@ -1,5 +1,23 @@
 import os
+import glob
 
-def checkversion():
-    print("Checking Version")
-    # versionNumberFile = os.read("version_number.txt")
+class CheckVersion:
+    def __init__(self):
+        pass
+
+    def checking_version(self):
+        print("Getting a list of version_nuimber.txt")
+        file_list = glob.glob("**/version_number.txt",recursive=True)
+        print(file_list)
+        for version_file in file_list:
+            file = open(version_file)
+            version_sematics = file.readline()
+            versioning = version_sematics.split(".") # Major.Minor.Patch
+            print(version_sematics)
+        #print("Checking Version")
+
+
+
+    
+CV = CheckVersion()
+CV.checking_version()
