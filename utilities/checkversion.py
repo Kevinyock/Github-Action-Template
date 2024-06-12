5c970222c5ad29e3a7be098f4ef1fbe8f6c87b23
Kevin <Westminsterkevindo@gmail.com>
2024-06-11 23:51:14 -0700

    Print to logging

--- utilities/checkversion.py
+++ utilities/checkversion.py
@@ -4,6 +4,7 @@ import os
 import glob
 import pathlib
 import argparse
+import logging
 
 class CheckVersion:
 
@@ -15,8 +16,12 @@ class CheckVersion:
     main_branch_name = "origin/main"
     development_branch_name = None
 
+    logger = None
+
     def __init__(self):
-        print("Initialize Check Version Class")
+        self.logger = logging.getLogger(__file__)
+        self.logger.setLevel(logging.INFO)
+        self.logger.info("Initialize Check Version Class")
         self.get_main_and_developing_branch()
 
     def get_main_and_developing_branch(self):
@@ -24,32 +29,32 @@ class CheckVersion:
         self.main_branch = self.repo.commit(self.main_branch_name)
         self.development_branch = self.repo.commit(self.repo.active_branch)
 
-        print(self.main_branch)
-        print(self.development_branch)
+        self.logger.info(self.main_branch)
+        self.logger.info(self.development_branch)
 
     def checking_version(self):
-        print("Getting a list of version_nuimber.txt")
+        self.logger.info("Getting a list of version_nuimber.txt")
         file_list = self.get_filelist_of_version_number_txt()
-        print(file_list)
+        self.logger.info(file_list)
         for version_file in file_list:
             file = open(version_file)
             version_sematics = file.readline()
             versioning = version_sematics.split(".") # Major.Minor.Patch
-            print(version_sematics)
-        #print("Checking Version")
+            self.logger.info(version_sematics)
+        #self.logger.info("Checking Version")
 
     def get_filelist_of_version_number_txt(self):
         return glob.glob('**/version_number.txt',recursive=True)
     
     def split_version_semantic(self):
-        print("Getting Major Version")
-        print("Getting Minor Version")
-        print("Getting Patch Version")
+        self.logger.info("Getting Major Version")
+        self.logger.info("Getting Minor Version")
+        self.logger.info("Getting Patch Version")
         return
     
     def get_repo_directory(self) -> pathlib.Path:
         path = pathlib.Path(__file__).parent.parent
-        print(path)
+        self.logger.info(path)
         return path
 
 parser = argparse.ArgumentParser()
